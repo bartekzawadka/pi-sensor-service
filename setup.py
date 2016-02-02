7cd104929437cb9bd7c4b60e2c4e70c1c7eb49c6
git <kontakt@bartoszzawadka.pl>
2016-02-02 19:06:17 +0100

    Added init.d script, updated setup script and turned debug mode off

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='pi-sensor-service',
+      version='1.0',
+      description='Raspberry Pi DHT21 sensor (temp and humidity) data REST service',
+      author='Bartek Zawadka',
+      author_email='kontakt@bartoszzawadka.pl',
+      packages=['pi-sensor-service'])
